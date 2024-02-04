e627866f3b5d9c44a5888c93d0c66fc2ffeb885b
bingohuang <770581715@qq.com>
2024-02-04 14:08:30 +0800

    update hugo.toml and port

--- hugo-start.sh
+++ hugo-start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # @aliyun server
-# nohup hugo server -D --bind 0.0.0.0 --port 2022 --baseURL "http://42.120.6.212:2022" >> hugo.log 2>&1 &
+# nohup hugo server -D --bind 0.0.0.0 --port 2024 --baseURL "http://42.120.6.212:2024" >> hugo.log 2>&1 &
 
 # @hibingo.cn
-# nohup hugo server -D --bind 0.0.0.0 --port 2022 --baseURL "http://hibingo.cn" >> hugo.log 2>&1 &
+# nohup hugo server -D --bind 0.0.0.0 --port 2024 --baseURL "http://hibingo.cn" >> hugo.log 2>&1 &
 
 # @localhost
-# hugo server -D --bind 0.0.0.0 --port 2022 --baseURL "http://127.0.0.1:2022"
+# hugo server -D --bind 0.0.0.0 --port 2024 --baseURL "http://127.0.0.1:2024"
 
 # @debug
-nohup hugo server -D --bind 0.0.0.0 --baseURL "http://42.120.6.212:2022" --port 2022 >> hugo.log 2>&1 &
+nohup hugo server -D --bind 0.0.0.0 --baseURL "http://42.120.6.212:2024" --port 2024 >> hugo.log 2>&1 &
